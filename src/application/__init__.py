924bc40e504c39d4d044de0723ecc767d9ef47a3
Nathan Lucas <nathan@bnlucas.com>
2013-06-13 01:35:00 -0600

    Removed GAE code.

--- src/application/__init__.py
+++ src/application/__init__.py
@@ -5,7 +5,6 @@ Initialize Flask app
 from flask import Flask
 
 # from flask_debugtoolbar import DebugToolbarExtension
-from gae_mini_profiler import profiler, templatetags
 from werkzeug.debug import DebuggedApplication
 
 
@@ -14,10 +13,6 @@ app.config.from_object('application.settings')
 
 # Enable jinja2 loop controls extension
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
-
-@app.context_processor
-def inject_profiler():
-    return dict(profiler_includes=templatetags.profiler_includes())
 
 # Pull in URL dispatch routes
 import urls
@@ -28,6 +23,3 @@ import urls
 # Werkzeug Debugger (only enabled when DEBUG=True)
 if app.debug:
     app = DebuggedApplication(app, evalex=True)
-
-# GAE Mini Profiler (only enabled on dev server)
-app = profiler.ProfilerWSGIMiddleware(app)
