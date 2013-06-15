eca43f88286a060cbb842cb6b26034441664fc72
Nathan Lucas <nathan@bnlucas.com>
2013-06-15 05:22:18 -0500

    Update wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.abspath('.'), 'lib'))
 
-from src import application
+from application import application
 
 if __name__ == '__main__':
 	application.run()
